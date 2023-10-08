9d081b5aacbb6ff7fc2cf2e90f13e7c684d6d95c
Chiamaka Adilieme <40030168+amikable-tech@users.noreply.github.com>
2023-10-08 03:30:00 +0200

    Revert "tailwindCSS configuration files"

--- flairandbeauty/flairandbeauty/settings.py
+++ flairandbeauty/flairandbeauty/settings.py
@@ -38,26 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website.apps.WebsiteConfig',
-    'phonenumber_field',
-    'tailwind',
-    'theme',
-    'django_browser_reload'
+    'phonenumber_field'
 ]
-
-#tailwind css
-TAILWIND_APP_NAME = 'theme'
-
-#Internal IPS
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
-
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django_browser_reload.middleware.BrowserReloadMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -130,10 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'theme/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
